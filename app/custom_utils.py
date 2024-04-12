95f6d35a5e93bb7e23479f5c223add359b0b3b17
Ayush <114686508+1ayushp@users.noreply.github.com>
2024-04-12 16:57:20 +0000

    Changes to be committed: 	modified:   app/__pycache__/custom_utils.cpython-310.pyc 	modified:   app/custom_utils.py

--- app/custom_utils.py
+++ app/custom_utils.py
@@ -12,10 +12,26 @@ def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
 #     step = checkpoint["step"]
 #     return step
 
+# def load_checkpoint(checkpoint, model, optimizer):
+#     print("=> Loading checkpoint")
+#     model.load_state_dict(dict([(n, p) for n, p in checkpoint['state_dict'].items()]), strict=False)
+#     optimizer.load_state_dict(checkpoint["optimizer"])
+#     step = checkpoint["step"]
+#     return step
+
 def load_checkpoint(checkpoint, model, optimizer):
     print("=> Loading checkpoint")
-    model.load_state_dict(dict([(n, p) for n, p in checkpoint['state_dict'].items()]), strict=False)
-    optimizer.load_state_dict(checkpoint["optimizer"])
-    step = checkpoint["step"]
-    return step
+    model.load_state_dict(checkpoint['state_dict'], strict=False)
 
+    # Check if optimizer state dict exists in the checkpoint
+    if 'optimizer' in checkpoint:
+        try:
+            optimizer.load_state_dict(checkpoint["optimizer"])
+        except ValueError:
+            print("=> Error: Loaded optimizer state dict doesn't match the size of the current optimizer's group.")
+            print("=> Initializing optimizer with default parameters.")
+    else:
+        print("=> Warning: Optimizer state not found in the checkpoint. Initializing optimizer with default parameters.")
+
+    step = checkpoint.get("step", 0)  # Get the value of 'step' if it exists, otherwise default to 0
+    return step
