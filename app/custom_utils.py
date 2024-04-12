eea18ea76602893745431d16a43ad5a2c0314f48
Ayush <114686508+1ayushp@users.noreply.github.com>
2024-04-12 16:51:45 +0000

    Changes to be committed: 	modified:   app/__pycache__/custom_utils.cpython-310.pyc 	modified:   app/custom_utils.py

--- app/custom_utils.py
+++ app/custom_utils.py
@@ -5,9 +5,17 @@ def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
     torch.save(state, filename)
 
 
+# def load_checkpoint(checkpoint, model, optimizer):
+#     print("=> Loading checkpoint")
+#     model.load_state_dict(checkpoint["state_dict"])
+#     optimizer.load_state_dict(checkpoint["optimizer"])
+#     step = checkpoint["step"]
+#     return step
+
 def load_checkpoint(checkpoint, model, optimizer):
     print("=> Loading checkpoint")
-    model.load_state_dict(checkpoint["state_dict"])
+    model.load_state_dict(dict([(n, p) for n, p in checkpoint['state_dict'].items()]), strict=False)
     optimizer.load_state_dict(checkpoint["optimizer"])
     step = checkpoint["step"]
     return step
+
